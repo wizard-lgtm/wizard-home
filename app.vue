4361b0454557703c6a7c14869f7b7f55c806edf4
ziggywizard <rippiten23@gmail.com>
2024-10-03 02:25:38 +0300

    darkmode added

--- app.vue
+++ app.vue
@@ -1,5 +1,12 @@
 <template>
-  <div>
-	  <NuxtPage />
-  </div>
+  <html :class="{ dark: darkMode }">
+    <div class="min-h-[100vh] dark:bg-[#222831] text-[#ECDFCC] box-border">
+      <Navbar />
+      <NuxtPage />
+    </div>
+  </html>
 </template>
+<script setup>
+const dark = true;
+const darkMode = ref(dark);
+</script>
