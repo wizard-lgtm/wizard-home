c8d452cbd645a0f620fefdaa0b0f287737ad9b0e
ziggywizard <rippiten23@gmail.com>
2024-10-03 02:25:27 +0300

    updated

--- pages/index.vue
+++ pages/index.vue
@@ -1,3 +1,24 @@
+<script setup lang="ts">
+import WhoAmI from "~/components/WhoAmI.vue";
+</script>
+
 <template>
-	<h1>Welcome to Wizard's home</h1>
+  <div class="min-h-[100vh]">
+    <div class="min-h-[100vh] flex justify-center items-center flex-col gap-3">
+      <img
+        src="https://avatars.githubusercontent.com/u/173285554?v=4"
+        alt="wizard"
+        class="rounded-full shadow-xl w-[170px]"
+      />
+      <h1 class="text-4xl font-bold underline text-center">wizard-lgtm</h1>
+      <ul
+        class="flex gap-3 translate-y-3 transition-all duration-300 ease-in-out"
+      >
+        <li class="underline cursor-pointer">Ziglang</li>
+        <li class="underline cursor-pointer">Kernel dev</li>
+      </ul>
+    </div>
+    <WhoAmI />
+    <Footer />
+  </div>
 </template>
