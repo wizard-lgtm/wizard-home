5e53a6980fde78364473c881bb40558aed8fde9b
ziggywizard <rippiten23@gmail.com>
2024-10-03 02:25:16 +0300

    footer added

---
+++ components/Footer.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="sm:px-12 p-4">
+    <div
+      class="flex bg-slate-400 py-4 bg-opacity-5 justfiy-center text-center backdrop-blur-lg justify-between px-12 rounded-md"
+    >
+      <div class="cursor-pointer font-bold text-xl flex items-center">:3</div>
+      <div class="flex justify-end gap-8 items-center">
+        <div class="cursor-pointer hidden sm:block">Source</div>
+        <div class="cursor-pointer hidden sm:block">Posts</div>
+        <div
+          class="cursor-pointer bg-[#ECDFCC] text-[#222831] py-2 px-3 rounded-lg transition-all duration-300 ease-in-out hover:bg-opacity-70 md:block"
+        >
+          Contact Me
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
