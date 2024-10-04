fb89a4a5815935c8a35b761f097f9a7f496b7188
ziggywizard <rippiten23@gmail.com>
2024-10-04 22:20:14 +0300

    lenis scroll added

---
+++ plugins/lenis.client.js
@@ -0,0 +1,25 @@
+// plugins/lenis.client.js
+import Lenis from "@studio-freight/lenis";
+
+export default defineNuxtPlugin((nuxtApp) => {
+  const lenis = new Lenis({
+    duration: 1.2,
+    easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)),
+    direction: "vertical",
+    gestureDirection: "vertical",
+    smooth: true,
+    mouseMultiplier: 1,
+    smoothTouch: false,
+    touchMultiplier: 2,
+    infinite: false,
+  });
+
+  function raf(time) {
+    lenis.raf(time);
+    requestAnimationFrame(raf);
+  }
+
+  requestAnimationFrame(raf);
+
+  nuxtApp.provide("lenis", lenis);
+});
